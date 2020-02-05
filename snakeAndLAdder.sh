751c12695ed7651ec820297ffcb4be1c2cc09223
supriyaphalle <supriyaphalle@gmail.com>
2020-02-05 15:28:02 +0530

    ADDED:function for checking no play , ladder or snake

--- snakeAndLAdder.sh
+++ snakeAndLAdder.sh
@@ -3,9 +3,32 @@
 echo "Welcome to the Snake and Ladder Simulation"
 
 
-start_position=0;
+#CONSTANT
+NO_PLAY=0;
+LADDER=1
+SNAKE=2;
+START_POSITION=0;
 
 
+#Variables
+currentPosition=0
 
-  number=$((RANDOM%6+1))
+function playOption() {
+	option=$((RANDOM%3))
+
+	case $option in
+		$NO_PLAY)
+				currentPossiton=$currentPossion
+				;;
+		$LADDER)
+				currentPosition=$(($currentPosition+$die))
+				;;
+		$SNAKE)
+				currentPosition=$(($currentPosition-$die))
+	esac
+}
+
+
+ die=$((RANDOM%6+1))
 	echo $number
+playOption
